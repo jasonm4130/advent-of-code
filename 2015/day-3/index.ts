e51dff0b3023f28ebd3a08e3b23a6d2cb9326557
Jason Matthew <jasonm4130@gmail.com>
2023-03-26 08:42:06 +1100

    refactored day 3 for more DRY

--- 2015/day-3/index.ts
+++ 2015/day-3/index.ts
@@ -10,6 +10,22 @@ async function readInput() {
   return input;
 }
 
+function advancePosition(x: number, y: number, char: string) {
+  if (char === '^') {
+    return [x, y + 1];
+  }
+  if (char === 'v') {
+    return [x, y - 1];
+  }
+  if (char === '>') {
+    return [x + 1, y];
+  }
+  if (char === '<') {
+    return [x - 1, y];
+  }
+  return [x, y];
+}
+
 (async () => {
   const input = await readInput();
 
@@ -27,21 +43,14 @@ async function readInput() {
   // Loop over each character in the input
   for (const char of input) {
     // Move in the direction specified by the character
-    if (char === '^') {
-      y += 1;
-    }
-    if (char === 'v') {
-      y -= 1;
-    }
-    if (char === '>') {
-      x += 1;
-    }
-    if (char === '<') {
-      x -= 1;
-    }
+    const [newX, newY] = advancePosition(x, y, char);
 
     // Add the new house to the set
-    houses.add(`${x},${y}`);
+    houses.add(`${newX},${newY}`);
+
+    // Update the current position
+    x = newX;
+    y = newY;
   }
 
   console.log(`Part 1: ${houses.size}`);
@@ -63,37 +72,25 @@ async function readInput() {
   for (let i = 0; i < input.length; i += 1) {
     // Move in the direction specified by the character
     if (i % 2 === 0) {
-      if (input[i] === '^') {
-        y1 += 1;
-      }
-      if (input[i] === 'v') {
-        y1 -= 1;
-      }
-      if (input[i] === '>') {
-        x1 += 1;
-      }
-      if (input[i] === '<') {
-        x1 -= 1;
-      }
+      // Advance Santa
+      const [newX, newY] = advancePosition(x1, y1, input[i]);
 
       // Add the new house to the set
-      houses2.add(`${x1},${y1}`);
+      houses2.add(`${newX},${newY}`);
+
+      // Update the current position
+      x1 = newX;
+      y1 = newY;
     } else {
-      if (input[i] === '^') {
-        y2 += 1;
-      }
-      if (input[i] === 'v') {
-        y2 -= 1;
-      }
-      if (input[i] === '>') {
-        x2 += 1;
-      }
-      if (input[i] === '<') {
-        x2 -= 1;
-      }
+      // Advance Robo-Santa
+      const [newX, newY] = advancePosition(x2, y2, input[i]);
 
       // Add the new house to the set
-      houses2.add(`${x2},${y2}`);
+      houses2.add(`${newX},${newY}`);
+
+      // Update the current position
+      x2 = newX;
+      y2 = newY;
     }
   }
 
